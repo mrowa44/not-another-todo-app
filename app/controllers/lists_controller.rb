185364039748da373299a3099aab286ed300cc36
Justyna Rachowicz <justynarachowicz@gmail.com>
2015-05-03 20:03:22 +0200

    Lists views and controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,48 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:edit, :show, :update,  :destroy]
+
   def index
+    @lists = List.all
+    @list = List.new
+  end
+
+  def show
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      flash[:success] = 'List successfully created!'
+      redirect_to @list
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def show
+  def update
+    if @list.update(list_params)
+      flash[:success] = "List was successfully updated!"
+      redirect_to @list
+    else
+      render :edit
+    end
   end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+    def set_list
+      @list = List.find(params[:id])
+    end
+   
+    def list_params
+      params.require(:list).permit(:title)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503171117) do
+ActiveRecord::Schema.define(version: 20150503171456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 20150503171117) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "todos", force: :cascade do |t|
+    t.string   "task"
+    t.integer  "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "todos", ["list_id"], name: "index_todos_on_list_id", using: :btree
+
+  add_foreign_key "todos", "lists"
 end
