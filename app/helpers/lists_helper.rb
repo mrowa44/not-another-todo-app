5c9687d103197f9ad48b20cd34f4d39b8937cf59
Justyna Rachowicz <justynarachowicz@gmail.com>
2015-05-14 13:32:23 +0200

    Add undone todos count badge

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -2,4 +2,12 @@ module ListsHelper
   def display_todo_state(state)
     state ? '✔' : '✘'
   end
+  
+  def count_undone(todos)
+    count = 0
+    todos.each do |todo|
+      count += 1 if todo.state == false
+    end
+    count
+  end
 end
